21a6ac970e2ec3bd76f9aa54d493734ae5716e21
duyaj <duyaj@uci.edu>
2024-07-05 09:29:22 -0700

    restart

--- components/icons/star.js
+++
@@ -1,9 +0,0 @@
-const StarIcon = () => {
-  return (
-    <svg width={20} height={20} viewBox="0 0 24 24" fill="currentColor">
-      <path d="M12 1.984l2.797 7.672h7.172l-5.797 4.401 2.172 7.172-6.969-5.297-6.984 5.297 2.172-7.172-5.797-4.401h7.172z" />
-    </svg>
-  )
-}
-
-export default StarIcon
